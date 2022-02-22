0812b2429cacf9917e2300c40e117b8e284fb9ce
snoions <simonguoyt@gmail.com>
2022-02-22 10:51:39 -0800

    added MA5

--- util.py
+++ util.py
@@ -28,23 +28,22 @@ def merge_time_series(arr1, arr2) -> tf.float64 :
 def split_time_series(len_i,len_o, arr) -> (np.array, np.array):
     a = []
     b = []
-    for i in range(len(arr)-len_i-len_o):
-        a.append(arr[i: i+len_i])
+    for i in range(len(arr)-len_i-len_o + 1):
+        a.append(arr[i: i+len_i]) 
         b.append(arr[i+len_i:i+len_i+len_o])
     return (np.array(a), np.array(b))
 
-def split_time_series(len_i,len_o, arr) -> (np.array, np.array):
-    a = []
-    b = []
-    for i in range(len(arr)-len_i-len_o):
-        a.append(arr[i: i+len_i])
-        b.append(arr[i+len_i:i+len_i+len_o])
-    return (np.array(a), np.array(b))
+def moving_average(len_i, arr) -> np.array:
+    res = []
+    for i in range(len(arr)-len_i):
+        a = arr[i: i+len_i]
+        res.append(sum(a)/len(a))
+    return np.array(res)
 
 def split_time_series_disjoint(len_i,len_o, arr) -> (np.array, np.array):
     a = []
     b = []
-    for i in range(0, len(arr)-len_i-len_o,  len_o):
+    for i in range(0, len(arr)-len_i-len_o + 1,  len_o):
         a.append(arr[i: i+len_i])
         b.append(arr[i+len_i:i+len_i+len_o])
     return (np.array(a), np.array(b))
@@ -56,11 +55,11 @@ def batch_standardize(input):
         return (scaler.transform(i), scaler)
     return zip(*[ standardize_fn (i) for i in input])
 
-def batch_transform(input, scalers):
+def batch_transform(input, scalers) -> np.array:
     scale = lambda x,s : s.transform(x) if(len(x.shape)==2) else s.transform([x])[0]
     return np.array([scale(x, s) for x,s in zip(input, scalers)])
 
-def batch_inverse_transform(input, scalers):
+def batch_inverse_transform(input, scalers) -> np.array:
     scale = lambda x,s : s.inverse_transform(x) if(len(x.shape)==2) else s.inverse_transform([x])[0]
     return np.array([scale(x, s) for x,s in zip(input, scalers)])
 
@@ -92,18 +91,20 @@ def AR(real_o,pred_o):
 
 
 #tests
-# (input, output) = split_time_series(5, 5, load_data("./data_stock/SP500_average.csv"))
-# (djt_input, djt_output) = split_time_series_disjoint(5, 5, load_data("./data_stock/SP500_average.csv"))
+# data = oad_data("./data_stock/SP500_average.csv")
+# (input, output) = split_time_series(5, 5, data)
+# (djt_input, djt_output) = split_time_series_disjoint(5, 5, data)
 # print(f'input shape, {input.shape}, output shape, {output.shape}')
 # merged = merge_time_series(input, output)
 # standardized = batch_standardize(input)
+# MA5 = moving_average(data)
 # print("input",input)
 # print("output", output)
 # print("djt_input",djt_input)
 # print("djt_input", djt_output)
 # print("merged",merged)
-# print("flattened input", flatten(input))
 # print("standardized", standardized)
+# print("MA5", MA5)
 
 
 
